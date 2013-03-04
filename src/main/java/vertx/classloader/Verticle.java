deb22a1a4e7bd4027ffe53c1e71a184713ff53e0
Son <leson.phung@gmail.com>
2013-03-04 20:27:00 +0800

    Dummy start of a vertx server

--- src/main/java/vertx/classloader/Verticle.java
+++ src/main/java/vertx/classloader/Verticle.java
@@ -1,12 +1,20 @@
 package vertx.classloader;
 
+import org.vertx.java.core.Handler;
+import org.vertx.java.core.http.HttpServerRequest;
+
 public class Verticle extends org.vertx.java.platform.Verticle {
 
 	@Override
 	public void start() throws Exception {
 		Singleton.getInstance();
 
-		vertx.createHttpServer().listen(8080);
+		vertx.createHttpServer()
+				.requestHandler(new Handler<HttpServerRequest>() {
+					@Override
+					public void handle(HttpServerRequest event) {
+					}
+				}).listen(8080);
 	}
 
 }
