ded007ffd9c9d88d2aaff62ec1d44bda6f5124a8
Son <leson.phung@gmail.com>
2013-03-04 20:24:22 +0800

    Create a vertx http server

--- src/main/java/vertx/classloader/Verticle.java
+++ src/main/java/vertx/classloader/Verticle.java
@@ -5,6 +5,8 @@ public class Verticle extends org.vertx.java.platform.Verticle {
 	@Override
 	public void start() throws Exception {
 		Singleton.getInstance();
+
+		vertx.createHttpServer().listen(8080);
 	}
 
 }
